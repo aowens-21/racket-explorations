3b1d801e0fcc8e7ecb89d83a078ac3b47540c032
Alex Owens <aowens5189@gmail.com>
2018-10-21 22:55:30 -0400

    More languages from beautiful racket book

--- languages/jsonic/expander.rkt
+++ languages/jsonic/expander.rkt
@@ -1,5 +1,4 @@
 #lang br/quicklang
-
 (require json)
 
 
--- languages/jsonic/jsonic-test.rkt
+++ languages/jsonic/jsonic-test.rkt
@@ -2,13 +2,8 @@
 
 // a line comment
 [
-  @$ 'null $@,
-  @$ (* 6 7) $@,
-  @$ (= 2 (+ 1 1)) $@,
-  @$ (list "array" "of" "strings") $@,
-  [
-    @$ (hash 'key-1 'null
-           'key-2 (even? 3)
-           'key-3 (hash 'subkey 21)) $@
-  ]
+  "foo",
+  // comment
+  @$ 42 $@,
+  @$ (* 8 8) $@
 ]
